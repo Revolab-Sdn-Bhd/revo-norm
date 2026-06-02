6345716815e4b11f1d474cbf83e602e48e7f7b30
aisyah-revo <aisyah.razak@revolab.ai>
2026-06-02 10:23:02 +0800

    fix for decimal number

--- revo_norm/normalizer_en.py
+++ revo_norm/normalizer_en.py
@@ -175,7 +175,7 @@ _time_re = re.compile(
     re.IGNORECASE,
 )
 _time_no_meridian_re = re.compile(
-    r"\b(\d{1,2})[:\.](\d{2})\b(?!\s*(?:am|pm|a\.m\.|p\.m\.))(?!.*%)",
+    r"\b(\d{1,2}):(\d{2})\b(?!\s*(?:am|pm|a\.m\.|p\.m\.))(?!.*%)",
     re.IGNORECASE,
 )
 
